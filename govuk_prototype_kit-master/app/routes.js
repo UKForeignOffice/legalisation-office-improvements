93191fa5f2e4a32d5f20a8d5f80165afe677da5d
rp825 <rp825@york.ac.uk>
2015-08-24 14:10:25 +0100

    Fixing missing files issue

---
+++ govuk_prototype_kit-master/app/routes.js
@@ -0,0 +1,30 @@
+module.exports = {
+    bind : function (app) {
+
+        app.get('/', function (req, res) {
+            res.render('index');
+        });
+
+        app.get('/examples/template-data', function (req, res) {
+            res.render('examples/template-data', { 'name' : 'Foo' });
+        });
+
+        // add your routes here
+        app.get('application_form_start', function (req, res) {
+            res.render('application_form_start', {'message' : 'Hello world' , 'text' : 'Oh dear'});
+        });
+
+        app.get('application_form_address', function (req, res) {
+            res.render('application_form_address', {'message' : 'Hello world' , 'text' : 'Oh dear'});
+        });
+
+        app.get('application_form_payment', function (req, res) {
+            res.render('application_form_payment', {'message' : 'Hello world' , 'text' : 'Oh dear'});
+        });
+
+        app.get('application_form_fail_address', function (req, res) {
+            res.render('application_form_fail_address', {'message' : 'Hello world' , 'text' : 'Oh dear'});
+        });
+
+    }
+};
