771e7a33bf98c4d9a40d889595af4384c138fdaa
rp825 <rp825@york.ac.uk>
2015-08-26 13:01:52 +0100

    Changes to Prototype: 25/08/15

--- Prototype/govuk_prototype_kit-master/app/routes.js
+++ Prototype/govuk_prototype_kit-master/app/routes.js
@@ -18,8 +18,8 @@ module.exports = {
             res.render('application_form_address', {'message' : 'Hello world' , 'text' : 'Oh dear'});
         });
 
-        app.get('application_form_payment', function (req, res) {
-            res.render('application_form_payment', {'message' : 'Hello world' , 'text' : 'Oh dear'});
+        app.get('payment', function (req, res) {
+            res.render('payment', {'message' : 'Hello world' , 'text' : 'Oh dear'});
         });
 
         app.get('application_form_fail_address', function (req, res) {
