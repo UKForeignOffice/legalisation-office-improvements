8b02d550b3f5bb9d20c98f4f20a73bc52beeb368
rp825 <rp825@york.ac.uk>
2015-08-27 11:10:49 +0100

    Email Verification

--- Prototype/govuk_prototype_kit-master/app/assets/javascripts/application.js
+++ Prototype/govuk_prototype_kit-master/app/assets/javascripts/application.js
@@ -248,3 +248,46 @@ $('#otherCountries').typeahead({
         }
     }
 });
+
+
+$("#phone").intlTelInput({
+    defaultCountry: "auto",
+    geoIpLookup: function(callback) {
+        $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            callback(countryCode);
+        });
+    },
+    utilsScript: "public/javascripts/int-tel-num/utils.js" // just for formatting/placeholders etc
+});
+
+
+//Mailcheck code
+
+$.getScript("mailcheck/mailcheck.min.js", function(){
+
+});
+
+var domains = ['gmail.com', 'aol.com'];
+var secondLevelDomains = ['hotmail']
+var topLevelDomains = ["com", "net", "org"];
+
+var superStringDistance = function(string1, string2) {
+    // a string distance algorithm of your choosing
+}
+
+
+$('#email').on('blur', function() {
+    $(this).mailcheck({
+        domains: domains,                       // optional
+        secondLevelDomains: secondLevelDomains, // optional
+        topLevelDomains: topLevelDomains,       // optional
+        distanceFunction: superStringDistance,  // optional
+        suggested: function(element, suggestion) {
+            // callback code
+        },
+        empty: function(element) {
+            // callback code
+        }
+    });
+});
--- Prototype/govuk_prototype_kit-master/public/javascripts/int-tel-num/intlTelInput.js
+++ Prototype/govuk_prototype_kit-master/public/javascripts/int-tel-num/intlTelInput.js
@@ -35,7 +35,7 @@
             // display only these countries
             onlyCountries: [],
             // the countries at the top of the list. defaults to united states and united kingdom
-            preferredCountries: [ "us", "gb" ],
+            preferredCountries: [ "gb", "fr", "es" ],
             // specify the path to the libphonenumber script to enable validation/formatting
             utilsScript: ""
         }, keys = {
